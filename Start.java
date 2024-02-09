c2edbe64a7fe9c723f6099c00bfa81508434f2e0
ass97 <maciek.m2004@wp.pl>
2024-02-09 16:13:46 +0100

    Reversing first

---
+++ Start.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class Start {
+    public static void main(String[] args) {
+       
+        JFrame frame = new JFrame("Text reverse app");
+        
+
+
+       
+    }
+}
---
+++ StringReverse.java
@@ -0,0 +1,30 @@
+public class StringReverse {
+    private String text;
+
+    public StringReverse(String text){
+        this.text = text;
+    }
+    public void setText(String text) {
+        this.text = text;
+    }
+    public String getText() {
+        return text;
+    }
+    
+    private String Reverse(){
+        
+        int length = text.length();
+        String reversed = "";
+
+        for (int i = 0; i < text.length(); i++) {
+            char temp = text.charAt(length-1);
+            length--;
+            reversed = reversed+ String.valueOf(temp);
+        }
+        return reversed;
+    }
+
+    public String toString(){
+        return "Your text was:"+text+"\nYour reversed text is "+Reverse();
+    }
+}
